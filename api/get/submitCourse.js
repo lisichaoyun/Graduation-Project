bc21a23044f66a5d1d995c5fa4e38568e5beff3a
lisichaoyun <lisichaoyun@163.com>
2022-03-09 20:27:24 +0800

    2022.9.9

--- api/get/submitCourse.js
+++ api/get/submitCourse.js
@@ -1,17 +1,37 @@
-var express=require('express')
-var api=express.Router()
-var sql=require('../../db')
-api.get('/',(req,res,next)=>{
-    if(req.session.username===undefined){
-      res.json({err:1,msg:'请先登录'})
-      next()
-    }
-    sql.query('SELECT selectedCourse FROM userinfo',[req.session.username])
-    .then(r=>{
-      res.json({err:0,msg:r})
+var express = require("express");
+var api = express.Router();
+var sql = require("../../db");
+api.get("/", (req, res, next) => {
+  if (req.session.username === undefined) {
+    res.json({ err: 1, msg: "请先登录" });
+    next();
+  }
+  let username = req.session.username;
+  let course = req.query.course;
+  /*关于promiss嵌套问题
+      使用多个then下一个then返回的是上一个then返回的promiss值（箭头函数指向promiss）
+      也可以使用await逐步执行promiss
+    */
+  sql
+    .query("SELECT selectedCourse FROM userinfo WHERE username = ?", [username])
+    .then((result) => {//查询原来加入内容
+      let resultArr = result[0].selectedCourse.split(" ");
+      resultArr.push(course);
+      return sql.query(
+        "UPDATE userinfo SET selectedCourse = ? WHERE username = ?",
+        [resultArr.join(" ").trim(), username]
+      );
     })
-    .catch(e=>{
-      res.json({err:1,msg:'账号查询错误'})
+    .then((result) => {
+      //返回结果
+      res.json({ err: 0, msg: result });
     })
-})
-module.exports=api
+    .catch((e) => {
+      res.json({ err: 1, msg: e });
+    })
+    .finally(() => {
+      next();
+    });
+});
+
+module.exports = api;
--- app.js
+++ app.js
@@ -1,16 +1,19 @@
+//引入基本模块
 var express = require("express");
 var app = express();
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+
+//引入与使用session与redis关联模块
 var redis = require("redis");//请安装老版本的redis@3.1.2
 var session = require("express-session");
 var RedisStore = require("connect-redis")(session);
 var client = redis.createClient(); //这里可以填写redis密码
-var storet=new RedisStore({
+var appstore=new RedisStore({
   client: client,
   port: 6379, //端口号
   host: "127.0.0.1",
-  ttl: 1 * 60 * 60 * 1000 //主机
+  ttl: 60 * 60 * 60 * 1000 //主机
 })
 app.use(
   session({
@@ -20,22 +23,36 @@ app.use(
       // domain: 'xxx.xxx.xxx.xxx:xxxx', // 域名
       path: "/",//这里secure属性限制https访问权限
       httpOnly: false, // 开启后前端无法通过 JS 操作
-      maxAge: 1 * 60 * 60 * 1000, // 这一条 是控制 sessionID 的过期时间的！！！
+      maxAge: 60 * 60 * 60 * 1000, // 这一条 是控制 sessionID 的过期时间的！！！
     }, //设置maxAge是80000ms，即80s后session和相应的cookie失效过期
     resave: false,
     saveUninitialized: true,
-    store: storet
+    store: appstore
   })
 );
+
+//基本模块的使用
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser()); //这里可以设置cookie加密字符串
-app.use("/api/SendEmailCode", require("./api/get/SendEmailCode"));
-app.use("/api/Selectcourse", require("./api/get/Selectcourse"));
-app.use("/api/courseNum", require("./api/get/courseNum"));
-app.use("/api/studentInfo", require("./api/get/studentInfo"));
-app.use("/api/login", require("./api/post/login"));
-app.use("/api/submitCourse", require("./api/get/submitCourse"));
-app.use("/api/register", require("./api/post/register"));
+
+//接口变量声明
+var SendEmailCode=require("./api/get/SendEmailCode")
+var Selectcourse=require("./api/get/Selectcourse")
+var courseNum=require("./api/get/courseNum")
+var studentInfo=require("./api/get/studentInfo")
+var login=require("./api/post/login")
+var submitCourse=require('./api/get/submitCourse')
+var register=require("./api/post/register")
+
+//引用接口
+app.use("/api/SendEmailCode", SendEmailCode);//注册时发送验证码
+app.use("/api/Selectcourse", Selectcourse);//返回可选课程
+app.use("/api/courseNum", courseNum);//返回课程数目
+app.use("/api/studentInfo", studentInfo);//返回学生信息
+app.use("/api/login", login);//登录接口
+app.use("/api/submitCourse", submitCourse);//提交选择课程接口
+app.use("/api/register", register);//注册接口
+//暴露主模块
 module.exports = app;
